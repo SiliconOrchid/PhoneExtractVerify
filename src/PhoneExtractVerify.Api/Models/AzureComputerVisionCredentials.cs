68b0f485d38c65e01085fb264c9a221671d92c49
Jim <jimmcgowan.uk@gmail.com>
2018-10-11 11:17:00 +0100

    Appsetting models

---
+++ src/PhoneExtractVerify.Api/Models/AzureComputerVisionCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace PhoneExtractVerify.Api.Models
+{
+    public class AzureComputerVisionCredentials
+    {
+        public string UriBase { get; set; }
+        public string SubscriptionKey { get; set; }
+    }
+}
---
+++ src/PhoneExtractVerify.Api/Models/TwilioCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace PhoneExtractVerify.Api.Models
+{
+    public class TwilioCredentials
+    {
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+    }
+}
